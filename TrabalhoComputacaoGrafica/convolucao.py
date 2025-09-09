09891e5d9b5add04572e183cfda15fbcd5141022
LucasCosta <156377381+Luda-costa@users.noreply.github.com>
2025-09-09 11:13:12 -0300

    Update convolucao.py

--- TrabalhoComputacaoGrafica/convolucao.py
+++ TrabalhoComputacaoGrafica/convolucao.py
@@ -65,5 +65,5 @@ def convolucao(img_path, kernel):
     return 0
 
 #kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-#convolucao('/home/luda/PycharmProjects/ComputacaoGrafica/atitus1.png', kernel)
+#convolucao('templo.jpeg', kernel)
 #
